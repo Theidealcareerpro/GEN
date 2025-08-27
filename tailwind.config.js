52134e0b356a316ecc5ecf896bd4de6abee68436
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-27 18:00:17 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
