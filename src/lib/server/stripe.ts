118ebc666331acc874ef45dd3c5219dffd69667e
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-28 16:46:32 +0100

    Update stripe.ts

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe";
 import { env } from "@/lib/env";
 
 export const stripe = env.STRIPE_SECRET_KEY
-  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2025-08-28" })
+  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2025-08-28.basil" })
   : (null as unknown as Stripe);
